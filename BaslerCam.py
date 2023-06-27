bca8484e98a7f44419aa47515a6d9dfabdb07ad8
ElhananBY <elhananby@gmail.com>
2023-06-27 12:11:16 +0200

    Camera class

--- BaslerCam.py
+++ BaslerCam.py
@@ -7,7 +7,8 @@ from dataclasses import dataclass
 from queue import Queue
 from typing import Any
 
-from pypylon import pylon
+import numpy as np
+from pypylon import genicam, pylon
 from vidgear.gears import WriteGear
 
 
@@ -25,19 +26,60 @@ class DataPacket:
     metadata: MetaData
 
 
-class BaslerCam(mp.Process):
-    def __init__(self, serial, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+class VideoWriter:
+    def __init__(
+        self,
+        params: dict,
+        data: Queue | None | list | np.ndarray,
+        filename: str | None,
+    ) -> None:
+        self.params = params
+        self.data = data
+        self.filename = filename
 
+        if self.filename is None:
+            raise ValueError("Must give a proper filename.")
+
+        self.writer = WriteGear(output=filename, logging=False, **self.params)
+
+    def run(self) -> None:
+        # Check what type of data we got
+        if isinstance(self.data, Queue.queue):
+            self.looped_video_writer()
+        elif isinstance(self.data, list) or isinstance(self.data, np.ndarray):
+            self.write_video()
+        else:
+            raise ValueError("Must give a proper data type.")
+
+    def write_video(self) -> None:
+        # write video immediatly
+        for frame in self.data:
+            self.writer.write(frame)
+        self.close()
+
+    def looped_video_writer(self) -> None:
+        try:
+            while True:
+                data = self.data.get()
+                if data is None:
+                    break
+                self.write_video(data.frames)
+        except KeyboardInterrupt:
+            pass
+        self.close()
+
+    def close(self) -> None:
+        self.writer.close()
+
+
+class BaslerCam:
+    def __init__(self, serial: str | int, camera_params: dict | None):
         # Get arguments
         self.serial = serial
-        self.args = args
-        self.kwargs = kwargs
 
         # Initialize camera
         self._initialize()
-        self._set_camera_parameter()
-        self._set_process_parameter()
+        self._set_camera_parameter(camera_params)
 
     def _initialize(self):
         """
@@ -46,50 +88,34 @@ class BaslerCam(mp.Process):
 
         # Initialize camera
         tlf = pylon.TlFactory.GetInstance()
-
-        # Get device for serial number
         info = pylon.DeviceInfo()
-        info.SetSerialNumber(self.serial)
+        info.SetSerialNumber(str(self.serial))
+        self.cam = pylon.InstantCamera(tlf.CreateDevice(info))
 
-        # start camera
-        self.cam = tlf.CreateDevice(info)
-
-    def _set_camera_parameter(self):
+    def _set_camera_parameter(self, params: dict | None):
         """
         Set the camera parameters
         """
 
         # Set camera parameters
         self.open()
+        for key, value in params.items():
+            try:
+                setattr(self.cam, key, value)
+                logging.debug(f"Setting {key} = {getattr(self.cam, key).Value}")
+            except genicam.LogicalErrorException:
+                logging.warning(f"Could not set {key} = {value}")
 
-        # Loop over the parameters of the camera, and change them if they exist in the input parameters
-        [
-            setattr(self.cam, key, value)
-            for key, value in self.kwargs.items()
-            if key in dir(self.cam)
-        ]
-        # for key in dir(self.cam):
-        #     if key in self.kwargs:
-        #         setattr(self.cam, key, self.kwargs[key])
-
-    def _set_process_parameter(self):
-        """
-        Set the process parameters
-        (barriers, queues, events, etc)
-        """
-
-        # Set process parameters
-        for key, value in self.kwargs.items():
-            setattr(self, key, value)
+    def _setup_video_writer(self):
+        # Start video writer thread
+        self.frame_queue = Queue()
+        self.video_writer = threading.Thread(target=self.threaded_video_writer).start()
 
     def run(self):
         """
         Main wrapper to start camera recording
         """
-
-        # Start video writer thread
-        self.queue = Queue()
-        self.video_writer = threading.thread(target=self.threaded_video_writer).start()
+        self._setup_video_writer()
 
         # If the camera is set to free run, start the camera in free run mode
         if self.run_type == "free_run":
@@ -102,7 +128,7 @@ class BaslerCam(mp.Process):
 
         # If the camera is set to something else, raise an error
         else:
-            self.queue.put(None)
+            self.frame_queue.put(None)
             raise ValueError("Invalid run type")
 
     def free_run(self):
@@ -117,17 +143,17 @@ class BaslerCam(mp.Process):
         if hasattr(self, "barrier"):
             self.barrier.wait()
 
+        # Start grabbing
+        self.cam.StartGrabbing(pylon.GrabStrategy_OneByOne)
+
         # Start looping until keyboard interrupt
         try:
             while True:
-                with self.cam.RetrieveResult(
-                    self.exposure_time, pylon.TimeoutHandling_ThrowException
-                ) as grabResult:
-                    frames.append(grabResult.GetArray())
+                frames.append(self.grab())
 
         # If there's a keyboard interrupt, put the frames into the queue
         except KeyboardInterrupt:
-            self.queue.put(DataPacket(frames, self.metadata))
+            self.frame_queue.put(DataPacket(frames, self.metadata))
             pass
 
         # Stop the camera
@@ -137,8 +163,13 @@ class BaslerCam(mp.Process):
         self.close()
 
         # Put None into the queue to signal the video writer to stop
-        self.queue.put(None)
-        self.video_writer.join()
+        self.frame_queue.put(None)
+
+        # try to close the video_writer if it's still alive
+        try:
+            self.video_writer.join()
+        except AttributeError:
+            pass
 
     def triggered_run(self):
         """
@@ -162,10 +193,7 @@ class BaslerCam(mp.Process):
                 trigger = self.trigger.is_set()
 
                 # Get frame from camera
-                with self.cam.RetrieveResult(
-                    self.exposure_time, pylon.TimeoutHandling_ThrowException
-                ) as grabResult:
-                    frames.append(grabResult.GetArray())
+                frames.append(self.grab())
 
                 # Check if trigger was set
                 if trigger:
@@ -178,7 +206,7 @@ class BaslerCam(mp.Process):
                     # And if the frame number is greater than the fps*time
                     # put the frames into the queue, and reset the frame number
                     if frames_number >= self.fps * self.time:
-                        self.queue.put(DataPacket(frames, self.metadata))
+                        self.frame_queue.put(DataPacket(frames, self.metadata))
                         frames_number = 0
                         trigger_set = 0
 
@@ -194,7 +222,7 @@ class BaslerCam(mp.Process):
         self.close()
 
         # Put None into the queue to signal the video writer to stop
-        self.queue.put(None)
+        self.frame_queue.put(None)
         self.video_writer.join()
 
     def threaded_video_writer(self):
@@ -210,7 +238,7 @@ class BaslerCam(mp.Process):
         # Loop, waiting for data packets to arrive to the video writer
         while True:
             # Get packet from queue
-            packet = self.queue.get()
+            packet = self.frame_queue.get()
 
             # Check if packet is a frame
             if packet is None:
@@ -221,11 +249,11 @@ class BaslerCam(mp.Process):
             metadata = packet.metadata
 
             # Setup the output filename
-            output_filename = "{:d}_obj_id_{:d}_cam_{:d}_frame_{:d}.mp4".format(
-                metadata["ntrig"],
-                metadata["obj_id"],
-                metadata["camera_serial"],
-                metadata["frame"],
+            output_filename = "{:d}_obj_id_{:d}_cam_{}_frame_{:d}.mp4".format(
+                metadata.ntrig,
+                metadata.obj_id,
+                metadata.camera_serial,
+                metadata.frame,
             )
 
             # Setup video
@@ -234,10 +262,11 @@ class BaslerCam(mp.Process):
                 logging=False,
                 **output_params,
             )
-
+            print("Writing to {}".format(output_filename))
             # Write frames to video
             for frame in frames:
                 video_writer.write(frame)
+            print("Finished writing to {}".format(output_filename))
 
     def _check_mp(self):
         """Make sure that the multiprocessing variables are available
@@ -249,6 +278,19 @@ class BaslerCam(mp.Process):
             return True
         else:
             raise ValueError("Triggered run requires event and barrier")
+
+    def grab(self):
+        """Acquire a frame from the camera
+
+        Returns:
+            _type_: _description_
+        """
+
+        # Grab frame from camera
+        with self.cam.RetrieveResult(
+            self.exposure_time + 100, pylon.TimeoutHandling_ThrowException
+        ) as grabResult:
+            return grabResult.GetArray()
 
     def open(self):
         """
@@ -267,3 +309,50 @@ class BaslerCam(mp.Process):
         Close the camera
         """
         self.cam.Close()
+
+    @property
+    def exposure_time(self):
+        return int(self.cam.ExposureTime.Value)
+
+
+class FreeRunBaslerCam(BaslerCam):
+    def __init__(self, serial: str | int, camera_params: dict | None):
+        super().__init__(serial, camera_params)
+
+    def run(self):
+        pass
+
+
+class TriggeredBaslerCam(BaslerCam):
+    def __init__(self, serial: str | int, camera_params: dict | None):
+        super().__init__(serial, camera_params)
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    cam = mp.Process(
+        target=BaslerCam,
+        args=(
+            "23047980",
+            {
+                "ExposureTime": 1900,
+                "SensorReadoutMode": "Fast",
+                "TriggerMode": "Off",
+                "tr": "0",
+            },
+        ),
+    )
+    cam.start()
+
+    print("Running")
+    try:
+        while True:
+            time.sleep(1 / 500)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt")
+        pass
+    cam.join()
