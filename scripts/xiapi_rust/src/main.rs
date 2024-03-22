a18495a5155f68a5a80957d107fee605360ff21a
Elhanan Buchsbaum <elhananby@gmail.com>
2024-03-22 16:20:29 +0100

    got the rust script working i think?

--- scripts/xiapi_rust/src/main.rs
+++ scripts/xiapi_rust/src/main.rs
@@ -1,43 +1,70 @@
-/*
- * Copyright (c) 2022. XIMEA GmbH - All Rights Reserved
- */
-//use xiapi_sys::XI_IMG_FORMAT;
-
+use clap::Parser;
 use std::collections::VecDeque;
 use std::time::{Duration, Instant};
-use xiapi::{xiSetParamFloat, Image, XI_IMG_FORMAT, XI_PRM_EXPOSURE};
+use xiapi;
+
+use ctrlc;
+use zmq;
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value = "1000")]
+    exposure: f32,
+
+    #[arg(long, default_value = "400")]
+    fps: f32,
+
+    #[arg(long, default_value = "10")]
+    runtime: u32,
+
+    #[arg(long, default_value = "2496")]
+    width: u32,
+
+    #[arg(long, default_value = "2048")]
+    height: u32,
+}
 
 fn main() -> Result<(), i32> {
-    let mut deque: VecDeque<Image<'_, u8>> = VecDeque::with_capacity(1000);
+    // Create a boolean flag to indicate if Ctrl+C has been pressed
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
 
+    // Register a handler for Ctrl+C signals
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    // Create a ZMQ context and socket
+    let ctx = zmq::Context::new();
+    let socket = ctx.socket(zmq::STREAM).unwrap();
+    socket.bind("tcp://*:5555").unwrap();
+    let mut msg = zmq::Message::new();
+
+    // Argument parser
+    let args = Args::parse();
+
+    // Create a deque to store images
+    let mut pre_buffer: VecDeque<xiapi::Image<'_, u8>> = VecDeque::with_capacity(args.fps as usize);
+    let mut post_buffer: VecDeque<xiapi::Image<'_, u8>> =
+        VecDeque::with_capacity(args.fps as usize * 2);
+    let mut switch = false;
     let mut iteration_times: Vec<Duration> = Vec::new();
+
+    // Open the camera
     let mut cam = xiapi::open_device(None)?;
 
-    let fps = 400; // frames per second
-    let runtime = 10; // seconds
-    let width = 2496;
-    let height = 2496;
-    println!(
-        "Starting acquisition for {} seconds at {} fps",
-        runtime, fps
-    );
-
-    unsafe {
-        xiSetParamFloat(
-            *cam,
-            XI_PRM_EXPOSURE.as_ptr() as *const ::std::os::raw::c_char,
-            5000.0,
-        );
-    }
-
     // Set camera parameters
-    cam.set_exposure(1000.0)?;
-    cam.set_image_data_format(XI_IMG_FORMAT::XI_MONO8)?;
+    cam.set_exposure(args.exposure)?;
+    cam.set_image_data_format(xiapi::XI_IMG_FORMAT::XI_MONO8)?;
     cam.set_acq_timing_mode(xiapi::XI_ACQ_TIMING_MODE::XI_ACQ_TIMING_MODE_FRAME_RATE_LIMIT)?;
-    cam.set_framerate(fps as f32)?;
-    // cam.set_acq_timing_mode(xiapi::XI_ACQ_TIMING_MODE::XI_ACQ_TIMING_MODE_FREE_RUN)?;
-    cam.set_height(height)?;
-    cam.set_width(width)?;
+    cam.set_framerate(args.fps)?;
+    cam.set_height(args.height)?;
+    cam.set_width(args.width)?;
 
     // Set camera bandwidth
     cam.set_limit_bandwidth(cam.limit_bandwidth_maximum()?)?;
@@ -47,28 +74,63 @@ fn main() -> Result<(), i32> {
 
     // Start acquisition
     let buffer = cam.start_acquisition()?;
+    println!("Acquisition started");
 
-    let iterations = fps * runtime;
-    for _i in 0..iterations {
+    while running.load(Ordering::SeqCst) {
+        // Start time
         let start_time = Instant::now();
+
+        // Attempt to receive a message from the socket
+        match socket.recv(&mut msg, zmq::DONTWAIT) {
+            Ok(_) => {
+                if msg.as_str().unwrap() == "stop" {
+                    break;
+                } else if msg.as_str().unwrap() == "start" {
+                    switch = true;
+                } else {
+                    // pass
+                }
+            }
+            Err(_) => {
+                // do nothing
+            }
+        }
+
+        // Get the next image from the camera
         let image = buffer.next_image::<u8>(None)?;
 
-        deque.push_back(image);
+        // Add frames to the appropriate buffer
+        if !switch {
+            pre_buffer.push_back(image);
+        } else if post_buffer.len() < post_buffer.capacity() {
+            post_buffer.push_back(image);
+        } else {
+            println!("Second buffer is full");
 
+            // clear both buffers
+            pre_buffer.clear();
+            post_buffer.clear();
+            switch = false;
+        }
+
+        // end time
         let end_time = Instant::now();
-
-        let duration = end_time - start_time;
-        iteration_times.push(duration);
+        let elapsed_time = end_time.duration_since(start_time);
+        iteration_times.push(elapsed_time);
     }
 
-    // Calculate average time
-    let total_duration: Duration = iteration_times.iter().sum();
-    let average_duration = total_duration / iterations as u32;
+    // print iteration time
+    println!(
+        "Average iteration time: {:?}",
+        iteration_times.iter().sum::<Duration>() / iteration_times.len() as u32
+    );
 
-    // Print average time
-    println!("Total time: {:?}", total_duration);
-    println!("Average time per iteration: {:?}", average_duration);
-    println!("FPS = {:?}", 1.0 / average_duration.as_secs_f64());
+    // print average fps
+    println!(
+        "Average FPS: {:?}",
+        1.0 / (iteration_times.iter().sum::<Duration>().as_secs_f64()
+            / iteration_times.len() as f64)
+    );
 
     Ok(())
 }
