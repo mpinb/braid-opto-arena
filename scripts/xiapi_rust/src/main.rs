381bd14e55b72f8d7ea7ca9532e1212e6f72462f
Elhanan Buchsbaum <elhananby@gmail.com>
2024-03-26 09:40:53 +0100

    more work on rust and python ximea scripts

--- scripts/xiapi_rust/src/main.rs
+++ scripts/xiapi_rust/src/main.rs
@@ -1,13 +1,17 @@
 use clap::Parser;
-use std::collections::VecDeque;
-use std::time::{Duration, Instant};
-use xiapi;
-
 use ctrlc;
-use zmq;
-
+use std::collections::VecDeque;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::time::{Duration, Instant};
+use zmq;
+
+struct ImageData {
+    frame: Vec<u8>,
+    timestamp_raw: u64,
+    nframe: u32,
+    acq_nframe: u32,
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -28,7 +32,8 @@ struct Args {
     height: u32,
 }
 
-fn main() -> Result<(), i32> {
+#[tokio::main]
+async fn main() -> Result<(), i32> {
     // Create a boolean flag to indicate if Ctrl+C has been pressed
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
@@ -46,12 +51,15 @@ fn main() -> Result<(), i32> {
     let mut msg = zmq::Message::new();
 
     // Argument parser
-    let args = Args::parse();
+    let args: Args = Args::parse();
+    let exposure = args.exposure;
+    let fps = args.fps;
+    let width = args.width;
+    let height = args.height;
 
     // Create a deque to store images
-    let mut pre_buffer: VecDeque<xiapi::Image<'_, u8>> = VecDeque::with_capacity(args.fps as usize);
-    let mut post_buffer: VecDeque<xiapi::Image<'_, u8>> =
-        VecDeque::with_capacity(args.fps as usize * 2);
+    let mut pre_buffer: VecDeque<Vec<u8>> = VecDeque::with_capacity(args.fps as usize);
+    let mut post_buffer: VecDeque<Vec<u8>> = VecDeque::with_capacity(args.fps as usize * 2);
     let mut switch = false;
     let mut iteration_times: Vec<Duration> = Vec::new();
 
@@ -59,18 +67,26 @@ fn main() -> Result<(), i32> {
     let mut cam = xiapi::open_device(None)?;
 
     // Set camera parameters
-    cam.set_exposure(args.exposure)?;
+    cam.set_exposure(exposure)?;
     cam.set_image_data_format(xiapi::XI_IMG_FORMAT::XI_MONO8)?;
     cam.set_acq_timing_mode(xiapi::XI_ACQ_TIMING_MODE::XI_ACQ_TIMING_MODE_FRAME_RATE_LIMIT)?;
-    cam.set_framerate(args.fps)?;
-    cam.set_height(args.height)?;
-    cam.set_width(args.width)?;
+    cam.set_framerate(fps)?;
+    cam.set_height(height)?;
+    cam.set_width(width)?;
 
     // Set camera bandwidth
     cam.set_limit_bandwidth(cam.limit_bandwidth_maximum()?)?;
     let buffer_size = cam.acq_buffer_size()?;
     cam.set_acq_buffer_size(buffer_size * 4)?;
     cam.set_buffers_queue_size(cam.buffers_queue_size_maximum()?)?;
+
+    // Create empty image struct
+    let mut image_data: ImageData = ImageData {
+        frame: Vec::new(),
+        timestamp_raw: 0,
+        nframe: 0,
+        acq_nframe: 0,
+    };
 
     // Start acquisition
     let buffer = cam.start_acquisition()?;
@@ -99,17 +115,24 @@ fn main() -> Result<(), i32> {
         // Get the next image from the camera
         let image = buffer.next_image::<u8>(None)?;
 
-        // Add frames to the appropriate buffer
-        if !switch {
-            pre_buffer.push_back(image);
-        } else if post_buffer.len() < post_buffer.capacity() {
-            post_buffer.push_back(image);
-        } else {
-            println!("Second buffer is full");
+        // Copy the image data to the image struct
+        image_data.frame = image.data().to_vec();
+        image_data.timestamp_raw = image.timestamp_raw();
+        image_data.nframe = image.nframe();
+        image_data.acq_nframe = image.acq_nframe();
 
-            // clear both buffers
+        // Push the image to the appropriate buffer
+        if !switch {
+            if pre_buffer.len() == pre_buffer.capacity() {
+                pre_buffer.pop_front();
+            }
+            pre_buffer.push_back(image_data.frame);
+        } else if post_buffer.len() < post_buffer.capacity() {
+            post_buffer.push_back(image_data.frame);
+        } else {
+            pre_buffer.append(&mut post_buffer);
             pre_buffer.clear();
-            post_buffer.clear();
+
             switch = false;
         }
 
